1e06aa6b1aab9af5973e0879fcee530e1529ca7a
Jacob Louis Lemus Peschel <sirrkitt@gmail.com>
2020-10-22 22:27:39 -0700

    Fix for setting uid/gid and fix permissions

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,20 @@ LABEL maintainer="Jacob Lemus Peschel <jacob@tlacuache.us>"
 ENV INIT="NO"
 ENV VERSION="3.5.7"
 
+ENV UID=500
+ENV GID=500
+
+ENV UID_POSTDROP=990
+ENV GID_POSTDROP=990
+
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod a+x /entrypoint.sh
 
 RUN apk update --no-cache && \
-	apk add -U --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main postfix postfix-ldap postfix-mysql postfix-pgsql postfix-sqlite
+	apk add -U --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main postfix postfix-ldap postfix-mysql postfix-pgsql postfix-sqlite postfix-pcre && \
+	chmod +x /entrypoint.sh
 
-VOLUME [ "/config", "/spool", "/ssl"]
+VOLUME [ "/config", "/data", "/ssl"]
 
 EXPOSE 25
 EXPOSE 465
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
-
 set -e
+/usr/sbin/deluser postfix
+/usr/sbin/delgroup postfix
+
+/usr/sbin/delgroup postdrop
+/usr/sbin/deluser vmail
+
+/usr/sbin/addgroup -S -g $GUID postfix
+/usr/sbin/adduser -S -H -h /var/spool/postfix -G postfix -g postfix -u $UID postfix
+/usr/sbin/addgroup postfix mail
+
+/usr/sbin/addgroup -S -g $GID_POSTDROP postdrop
+/usr/sbin/adduser -S -H -h /var/mail -s /sbin/nologin -G postdrop -u $UID_POSTDROP -g vmail vmail
 
 #check if config read/write
 	#or else die
+
 if [ ! -w "/config/" ]
 then
 	echo "Unable to read or write config directory!"
 	return 1
-
-elif [ "$INIT" == "YES" ]
-then
-	/bin/cp -R /etc/postfix/* /config/
 
 elif [ ! -e /config/main.cf ] || [ ! -e /config/master.cf ]
 then
 	echo "Missing config files!"
 fi
 
-/bin/chown -R root:root /config
-
+/bin/chown -R root:root /config /socket /ssl
+/bin/chown -R postfix:postfix /data
 exec /usr/sbin/postfix -c /config start-fg
